c0ff91a6c08fc30042eef9b54db06d3f8638ec82
albert.chen <albert.chen@m2micro.com>
2020-10-16 17:46:51 +0800

    创建项目

---
+++ vue/jingdong/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+import ant from 'ant-design-vue'
+Vue.config.productionTip = false
+
+Vue.use(ant)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
